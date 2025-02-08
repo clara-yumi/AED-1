b1c1327678290005e1d38f12372a3606541f3a18
clara-yumi <clara.yumi@unifesp.br>
2025-02-08 20:26:47 -0300

    1016.c

---
+++ 1016.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+ 
+int main() {
+    int X = 60;
+    int Y = 90;
+    int dst;
+    int T;
+    
+    scanf("%d", &dst);
+    
+    printf("%d minutos\n",dst*2);
+ 
+    return 0;
+}
